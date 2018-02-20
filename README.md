19c0dadf82fff83096399a7b41843c0f3f9c89b4
Seb2lyon <seb2lyon71@gmail.com>
2018-02-20 13:58:42 +0100

    Polish translation quite done

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # ClassGenerator
 
-ClassGenerator is a GUI program (in french) written in C++ with Qt library.
+ClassGenerator is a GUI program (in french or english) written in C++ with Qt library.
 
 This is a graphical tool that will help you to generate automatically a simple C++ class source code.
 
 The idea of this program come from the wonderfull website "Openclassrooms" which is made for beginners to learn programing langages. I've learned C++ from there : https://openclassrooms.com/courses/programmez-avec-le-langage-c
 
 I still have one little improvement to do :
-- translation in English and Polish
+- translation in Polish
 
 
 Don't hesitate to sent me coments, ideas or to improve this code by sending me some PR.
--- config/ClassGenerator_pl.ts
+++ config/ClassGenerator_pl.ts
@@ -6,54 +6,54 @@
     <message>
         <location filename="fenattributs.cpp" line="17"/>
         <source>Options de l&apos;attribut</source>
-        <translation type="unfinished"></translation>
+        <translation>Opcje atrybutów</translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="18"/>
         <source>Liste des attributs</source>
-        <translation type="unfinished"></translation>
+        <translation>Lista atrybutów</translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="19"/>
         <source>VALIDER</source>
-        <translation type="unfinished"></translation>
+        <translation>POTWIERDZENIE</translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="47"/>
         <source>&amp;Type : </source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Typ : </translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="48"/>
         <source>&amp;Nom : </source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Nazwa : </translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="49"/>
         <source>&amp;Pointeur : </source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Wskaźnik : </translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="50"/>
         <source>&amp;Vector : </source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Vector : </translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="51"/>
         <source>Visibilité : </source>
-        <translation type="unfinished"></translation>
+        <translation>Widoczność : </translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="56"/>
         <source>Annuler</source>
-        <translation type="unfinished"></translation>
+        <translation>Anulować</translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="59"/>
         <location filename="fenattributs.cpp" line="171"/>
         <location filename="fenattributs.cpp" line="210"/>
         <source>Ajouter</source>
-        <translation type="unfinished"></translation>
+        <translation>Dodać</translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="112"/>
@@ -61,41 +61,43 @@
         <location filename="fenattributs.cpp" line="245"/>
         <location filename="fenattributs.cpp" line="318"/>
         <source>Modifier</source>
-        <translation type="unfinished"></translation>
+        <translation>Zmiana</translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="114"/>
         <source>Supprimer</source>
-        <translation type="unfinished"></translation>
+        <translation>Usunąć</translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="116"/>
         <source>Tout supprimer</source>
-        <translation type="unfinished"></translation>
+        <translation>Usuń wszystko</translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="186"/>
         <location filename="fenattributs.cpp" line="193"/>
         <location filename="fenattributs.cpp" line="214"/>
         <source>Attention</source>
-        <translation type="unfinished"></translation>
+        <translation>Uwaga</translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="186"/>
         <source>Vous devez renseigner le nom de l&apos;attribut</source>
-        <translation type="unfinished"></translation>
+        <translation>Musisz podać nazwę atrybutu</translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="193"/>
         <source>Le nom de l&apos;attribut est invalide :
 Le nom ne doit pas commencer par un chiffre</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa atrybutu jest nieprawidłowa :
+Nazwa nie może zaczynać się od cyfry</translation>
     </message>
     <message>
         <location filename="fenattributs.cpp" line="214"/>
         <source>Le nom de l&apos;attribut est déjà pris
 Veuillez en saisir un nouveau</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa tego atrybutu już istnieje
+Wprowadź nowy</translation>
     </message>
 </context>
 <context>
@@ -103,32 +105,32 @@ Veuillez en saisir un nouveau</source>
     <message>
         <location filename="fencodegenere.cpp" line="34"/>
         <source>Fermer</source>
-        <translation type="unfinished"></translation>
+        <translation>Blisko</translation>
     </message>
     <message>
         <location filename="fencodegenere.cpp" line="36"/>
         <source>Enregistrer</source>
-        <translation type="unfinished"></translation>
+        <translation>Rekord</translation>
     </message>
     <message>
         <location filename="fencodegenere.cpp" line="68"/>
         <source>Enregistrer le fichier header</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapisz plik header</translation>
     </message>
     <message>
         <location filename="fencodegenere.cpp" line="68"/>
         <source>Header (*.h)</source>
-        <translation type="unfinished"></translation>
+        <translation>Header (*.h)</translation>
     </message>
     <message>
         <location filename="fencodegenere.cpp" line="72"/>
         <source>Enregistrer le fichier source</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapisz plik źródłowy</translation>
     </message>
     <message>
         <location filename="fencodegenere.cpp" line="72"/>
         <source>Code source (*.cpp)</source>
-        <translation type="unfinished"></translation>
+        <translation>kod źródłowy (*.cpp)</translation>
     </message>
 </context>
 <context>
@@ -136,32 +138,32 @@ Veuillez en saisir un nouveau</source>
     <message>
         <location filename="fenincludes.cpp" line="7"/>
         <source>Choisissez les &quot;includes&quot; que vous voulez faire apparaitre dans votre code</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz &quot;includes&quot;, które chcesz umieścić w swoim kodzie</translation>
     </message>
     <message>
         <location filename="fenincludes.cpp" line="29"/>
         <source>Sélectionnez les &quot;includes&quot; de la bibliothèque standard C++ que vous souhaitez utiliser : </source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz &quot;includes&quot; standardowej biblioteki C ++, której chcesz użyć : </translation>
     </message>
     <message>
         <location filename="fenincludes.cpp" line="31"/>
         <source>Ajouter &gt;&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Dodać &gt;&gt;</translation>
     </message>
     <message>
         <location filename="fenincludes.cpp" line="33"/>
         <source>&lt;&lt; Supprimer</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;&lt; Usunąć</translation>
     </message>
     <message>
         <location filename="fenincludes.cpp" line="35"/>
         <source>Annuler</source>
-        <translation type="unfinished"></translation>
+        <translation>Anulować</translation>
     </message>
     <message>
         <location filename="fenincludes.cpp" line="37"/>
         <source>VALIDER</source>
-        <translation type="unfinished"></translation>
+        <translation>POTWIERDZENIE</translation>
     </message>
 </context>
 <context>
@@ -169,98 +171,98 @@ Veuillez en saisir un nouveau</source>
     <message>
         <location filename="fenprincipale.cpp" line="26"/>
         <source>&amp;Nom : </source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Nazwa :</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="27"/>
         <source>Classe &amp;mère : </source>
-        <translation type="unfinished"></translation>
+        <translation>Klasa &amp;matki : </translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="30"/>
         <source>Définition de la classe</source>
-        <translation type="unfinished"></translation>
+        <translation>Definicja klasy</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="36"/>
         <source>Protéger le &amp;header contre les inclusions multiples</source>
-        <translation type="unfinished"></translation>
+        <translation>Chroń &amp;header przed wieloma włączeniami</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="40"/>
         <source>Générer un &amp;constructeur par défaut</source>
-        <translation type="unfinished"></translation>
+        <translation>Wygeneruj domyślny &amp;konstruktor</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="43"/>
         <source>Générer un &amp;destructeur</source>
-        <translation type="unfinished"></translation>
+        <translation>Wygeneruj &amp;destruktor</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="45"/>
         <source>Ajouter des &amp;includes</source>
-        <translation type="unfinished"></translation>
+        <translation>Dodaj &amp;includes</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="47"/>
         <source>Ajouter des &amp;attributs</source>
-        <translation type="unfinished"></translation>
+        <translation>Dodaj &amp;atrybuty</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="49"/>
         <source>&amp;Générer accesseurs</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Wygeneruj akcesory</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="66"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Opcje</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="74"/>
         <source>Aucune licence associée à ce logiciel</source>
-        <translation type="unfinished"></translation>
+        <translation>Brak licencji związanej z tym oprogramowaniem</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="78"/>
         <location filename="fenprincipale.cpp" line="225"/>
         <source>Autre... (modifiez cette ligne)</source>
-        <translation type="unfinished"></translation>
+        <translation>Inne ... (zmień tę linię)</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="84"/>
         <source>A&amp;uteur : </source>
-        <translation type="unfinished"></translation>
+        <translation>A&amp;utor : </translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="85"/>
         <source>Da&amp;te de création : </source>
-        <translation type="unfinished"></translation>
+        <translation>Data &amp;utworzenia : </translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="86"/>
         <source>&amp;Licence : </source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Licencja : </translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="87"/>
         <source>&amp;Rôle de la classe : </source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Rola klasy: : </translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="90"/>
         <source>Ajouter des commentaires</source>
-        <translation type="unfinished"></translation>
+        <translation>Dodaj komentarze</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="97"/>
         <source>Générer</source>
-        <translation type="unfinished"></translation>
+        <translation>Generowania</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="99"/>
         <source>Quitter</source>
-        <translation type="unfinished"></translation>
+        <translation>Opuszczać</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="174"/>
@@ -268,81 +270,84 @@ Veuillez en saisir un nouveau</source>
         <location filename="fenprincipale.cpp" line="188"/>
         <location filename="fenprincipale.cpp" line="195"/>
         <source>Attention</source>
-        <translation type="unfinished"></translation>
+        <translation>Uwaga</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="174"/>
         <source>Vous devez renseigner le nom de la Classe</source>
-        <translation type="unfinished"></translation>
+        <translation>Musisz wpisać nazwę klasy</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="181"/>
         <source>Le nom de la Classe est invalide :
 Le nom ne doit pas commencer par un chiffre</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa klasy jest nieprawidłowa :
+Nazwa nie może zaczynać się od cyfry</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="188"/>
         <source>Le nom de la Classe mère est invalide :
 Le nom ne doit pas commencer par un chiffre</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa klasa matki jest nieprawidłowa :
+Nazwa nie może zaczynać się od cyfry</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="195"/>
         <source>Le nom de la Classe mère est invalide :
 Le nom de la Classe mère doit être différent du nom de la Classe</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa klasa matki jest nieprawidłowa :
+Nazwa klasy matki musi być inna niż nazwa klasy</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="215"/>
         <source>Auteur : </source>
-        <translation type="unfinished"></translation>
+        <translation>Autor : </translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="220"/>
         <source>Date de création : </source>
-        <translation type="unfinished"></translation>
+        <translation>Data utworzenia : </translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="227"/>
         <source>
 Ce logiciel est sous licence : </source>
-        <translation type="unfinished"></translation>
+        <translation>To oprogramowanie jest licencjonowane : </translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="235"/>
         <source>
 Rôle :
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Rola : </translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="442"/>
         <source>// Constructeur
 </source>
-        <translation type="unfinished"></translation>
+        <translation>// Konstruktor</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="463"/>
         <source>// Destructeur
 </source>
-        <translation type="unfinished"></translation>
+        <translation>// Destruktor</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="490"/>
         <source>// Accesseurs
 </source>
-        <translation type="unfinished"></translation>
+        <translation>// Akcesory</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="606"/>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Informacja</translation>
     </message>
     <message>
         <location filename="fenprincipale.cpp" line="606"/>
         <source>&lt;strong&gt;CodeGenerator v. 4.3&lt;/strong&gt;&lt;br /&gt;&lt;br /&gt;Programmeur : Seb2lyon&lt;br /&gt;Développé entre le 30-01-2018 et le 05-03-2018&lt;br /&gt;GNU General Public License v3.0&lt;br /&gt;&lt;br /&gt;&lt;a href=http://seb2lyon.site11.com&gt;Visitez mon site web !!!&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;strong&gt;CodeGenerator v. 4.3&lt;/strong&gt;&lt;br /&gt;&lt;br /&gt;Programista : Seb2lyon&lt;br /&gt;Opracowany między 30.01.2018 a 05.03.2018&lt;br /&gt;GNU General Public License v3.0&lt;br /&gt;&lt;br /&gt;&lt;a href=http://seb2lyon.site11.com&gt;Odwiedź moją stronę internetowę !!!&lt;/a&gt;</translation>
     </message>
 </context>
 </TS>
